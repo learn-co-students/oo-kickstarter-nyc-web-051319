453c7911667e71c513b78beed20907cc62b7b71c
locky5 <daikevin19@yahoo.com>
2019-05-16 14:38:17 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/project.rb'
+
+class Backer
+  attr_accessor :backed_projects
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) # adds backer (self = the Backer instance) to project's @backers array
+  end
+
+end
